483b5bdfa5107a412382e59a1fcef702d5d50411
srinidhi2104 <srinidhi.susarla@bscedsh.christuniversity.in>
2026-02-03 15:23:18 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const getAsset = (path: string) => {
 const siteConfig = {
   personal: {
     name: 'Sri Nidhi',
-    title: 'STUDENT STUDENT',
+    title: 'Christ University Bsc Eco And Data cience Student',
     image: getAsset('images/photo.png'), // Customize or replace with your profile image
     description:
       'Write a short bio here describing your background and focus. Keep it to 2–4 sentences.\n' +
